5dd2f68446c0c70631d77ecc48b30fc6f6a001b2
398utubzyt <46660281+398utubzyt@users.noreply.github.com>
2021-07-25 11:04:29 -0700

    commands and shit

--- index.js
+++ index.js
@@ -1,48 +1,8 @@
 'use strict';
 
-//#region Classes and Shit
-
-/**
- * @class An internal class called Command, that holds information about a command.
- */
- class Command {
-    constructor(name, aliases, user, method) {
-        this.name = name;
-        this.aliases = aliases;
-        this.user = user;
-        this.method = method;
-    }
-}
-
-class CommandUtil {
-    static get Prefix() {
-        return ':';
-    }
-
-    /**
-     * 
-     * @param {Discord.Message} message
-     * @returns {Command} The command sent in the message
-     */
-    static MessageToCommand(message) {
-        if (typeof(message) != String) {
-            throw 'Message content is not a string.';
-        }
-
-        if (!message.content.startsWith(this.Prefix)) {
-            throw 'Message is not a command.';
-        }
-
-        var content = message.content.substr(this.Prefix.length, message.content.length - this.Prefix.length);
-        var args = content.split(' ');
-
-        return new Command();
-    }
-}
-
-//#endregion
-
 //#region Import/Require
+
+//#region Classes and Shit
 
 // This API is automatically gitignore-d.
 // When testing, just create your own file
@@ -53,6 +13,130 @@ const API = require('./api.json');
 const readline = require('readline').createInterface(process.stdin, process.stdout);
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+/**
+ * Sends a message in the specified channel.
+ * @param {string} channel 
+ * @param {string} message 
+ */
+function SendMessage(channel, message) {
+	client.guilds.fetch(API.serverId).then((guild) => {
+		var cache = guild.channels.cache.find((value, key, collection) => {return value.name == channel;});
+
+		if (cache == null) {
+			return;
+		}
+		
+		cache.send(message).catch((err) => error(err));
+	}).catch((err) => error(err));
+}
+
+/**
+ * @class An internal class called Command, that holds information about a command.
+ */
+ class Command {
+
+	//#region Command Functions
+
+	static Help(channel) {
+		SendMessage(channel, "__**Command List**__ \nHelp: The command you just ran, eediot. \nPing: See how slow the bot is running.");
+	}
+
+	static Ping(time, channel) {
+		client.guilds.fetch(API.serverId).then((guild) => {
+			var cache = guild.channels.cache.find((value, key, collection) => {return value.name == channel;});
+	
+			if (cache == null) {
+				return;
+			}
+			
+			cache.send('Pinging...').then((m) => {m.edit('Your ping is: ' + m.createdTimestamp - time + 'ms.')}).catch((err) => error(err));
+		}).catch((err) => error(err));
+	}
+
+	//#endregion
+
+	/**
+	 * 
+	 * @param {string} cmdName The name of the command
+	 * @param {string[]} args The arguments of the command
+	 * @param {Discord.Message} message The message that was sent
+	 */
+	static GetFromName(cmdName, args, message)
+	{
+		switch (cmdName) {
+			case 'help':
+				var arr = args;
+				return new Command('help', [], args, message, this.Help);
+			
+			case 'ping':
+				var arr = args;
+				arr.push(message.createdTimestamp);
+				return new Command('ping', [], args, message, this.Ping);
+
+			default:
+				SendMessage(message.channel.name, 'Invalid command!');
+				return null;
+		}
+	}
+
+	/**
+	 * Runs the command.
+	 */
+	Run() {
+		var arr = this.args;
+		if (arr.length > 0 && arr[0] == '') {
+			arr = arr.reverse();
+			arr.pop();
+			arr = arr.reverse();
+		}
+
+		arr.push(this.message.channel.name);
+
+		//SendMessage(this.message.channel.name, 'Running command: ' + this.name + ' with args [' + arr.join(', ') + '] called by ' + this.message.author.username + '.');
+		this.method.call(this, arr);
+	}
+
+	/**
+	 * Creates a command.
+	 * @param {string} name 
+	 * @param {string[]} aliases 
+	 * @param {string[]} args
+	 * @param {Discord.Message} message
+	 * @param {function} method 
+	 */
+    constructor(name, aliases, args, message, method) {
+        this.name = name;
+        this.aliases = aliases;
+		this.args = args;
+        this.message = message;
+        this.method = method;
+    }
+}
+
+class CommandUtil {
+    static get Prefix() {
+        return '::';
+    }
+
+    /**
+     * 
+     * @param {Discord.Message} message
+     * @returns {Command} The command sent in the message
+     */
+    static MessageToCommand(message) {
+        if (!message.content.startsWith(this.Prefix)) {
+            throw 'Message is not a command.';
+        }
+
+        var content = message.content.substr(this.Prefix.length);
+        var args = content.split(' ');
+
+        return Command.GetFromName(args[0].trim().toLowerCase(), content.substr(args[0].length).trimStart().split(' '), message);
+    }
+}
+
+//#endregion
 
 //#endregion
 
@@ -65,6 +149,12 @@ client.on('ready', () => {
 client.on('message', msg => {
 	if (msg.content.startsWith(CommandUtil.Prefix)) {
 		var command = CommandUtil.MessageToCommand(msg);
+		
+		if (command == null) {
+			return;
+		}
+
+		command.Run();
 	}
 });
 
@@ -82,15 +172,7 @@ readline.on('line', (line) => {
 		return;
 	}
 
-	client.guilds.fetch(API.serverId).then((guild) => {
-		var channel = guild.channels.cache.find((value, key, collection) => {return value.name == args[0];});
-
-		if (channel == null) {
-			return;
-		}
-		
-		channel.send(line.substring(args[0].length).trimStart()).catch((err) => error(err));
-	}).catch((err) => error(err));
+	SendMessage(args[0], line.substring(args[0].length).trimStart());
 });
 
 function error(err) {
